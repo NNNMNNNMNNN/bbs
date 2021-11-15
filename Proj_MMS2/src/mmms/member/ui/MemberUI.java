85fbecbc03deeccc9d951b2d9f828c589ea2ebfb
Cat-Hanbit <allon1034@gmil.com>
2021-11-15 12:11:35 +0900

    no message

--- Proj_MMS2/src/mmms/member/ui/MemberUI.java
+++ Proj_MMS2/src/mmms/member/ui/MemberUI.java
@@ -1,9 +1,31 @@
 package mmms.member.ui;
 
+import java.util.Scanner;
+
+import mmms.member.controller.MemberController;
+
 public class MemberUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		boolean isStop = false;
+		MemberController memberController = new MemberController();
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("=====회원관리 프로그램=====");
+			System.out.println('"1.회원등록");
+			System.out.println("2.회원목록보기");
+			System.out.println("3.회원정보 수정");
+			System.out.println("4.회원정보삭제");
+			System.out.println("5.프로그램종료");
+			System.out.println("메뉴번호 : ");
+			
+			
+
+
+
+		} while (!isStop);
+
 
 	}
 
